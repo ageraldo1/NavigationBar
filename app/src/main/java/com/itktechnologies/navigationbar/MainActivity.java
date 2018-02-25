5e822306ce756e964e5c037aa769bcd637706626
ageraldo1 <ageraldo@itktechnologies.com>
2018-02-25 17:01:38 -0500

    first commit

---
+++ app/src/main/java/com/itktechnologies/navigationbar/MainActivity.java
@@ -0,0 +1,13 @@
+package com.itktechnologies.navigationbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
