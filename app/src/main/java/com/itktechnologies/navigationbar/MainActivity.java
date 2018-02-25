e45e263cd0e0aa33dfdc052be880660f979144e9
ageraldo1 <ageraldo@itktechnologies.com>
2018-02-25 18:44:19 -0500

    Simple test

--- app/src/main/java/com/itktechnologies/navigationbar/MainActivity.java
+++ app/src/main/java/com/itktechnologies/navigationbar/MainActivity.java
@@ -1,5 +1,6 @@
 package com.itktechnologies.navigationbar;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -44,6 +45,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Intent intent;
 
         switch (item.getItemId()) {
             case R.id.nav_account:
@@ -52,6 +54,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
             case R.id.nav_settings:
                 Log.i("MenuItem", "Settings");
+                intent = new Intent(getApplicationContext(), Support.class);
+                startActivity(intent);
                 break;
 
             case R.id.nav_logout:
